b93cc58a20615cd6984dac78dcd4df2407940334
damien.ramsey@cpl.ie <\SourceCtrlSample>
2016-06-29 14:27:07 +0100

    change of form size

--- SourceCtrlSample/Form1.Designer.cs
+++ SourceCtrlSample/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 79);
+            this.ClientSize = new System.Drawing.Size(276, 74);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
