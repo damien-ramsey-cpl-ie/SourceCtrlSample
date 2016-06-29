d1094a245f491c7091b3e52613c771781fb07255
damien.ramsey@cpl.ie <\SourceCtrlSample>
2016-06-29 14:00:17 +0100

    Initial commit

--- SourceCtrlSample/Form1.cs
+++ SourceCtrlSample/Form1.cs
@@ -16,5 +16,10 @@ namespace SourceCtrlSample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string x = "damiens first change";
+        }
     }
 }
